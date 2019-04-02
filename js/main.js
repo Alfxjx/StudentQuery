5f8d07d39caa26e57e23ae09e93aa39850a58836
AlfredPC <xjxtju@163.com>
2019-04-02 15:24:46 +0800

    btn animate

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const d = document.getElementById('dropdown');
 const input = document.getElementById('prov');
 const button = document.querySelector('#show');
+// 飞入效果
 button.addEventListener('click', event=>{
     event.stopPropagation();
     if(d.style.display==="block"){
@@ -23,12 +24,19 @@ for(let i =0;i<items.length;i++){
         input.value = items[i].innerText;
     })    
 }
-
+// 点击触发查询按钮
 function query(){
     let province = document.getElementById('prov');
     let number = document.getElementById('Qnum');
-    console.log(province.value);
-    console.log(number.value);
+    let btn = document.getElementById('btn-ani');
+    btn.style.animation = 'input-shadow 0.1s ease';
+    btn.style.boxShadow = `0 3px 3px 0 rgba(0, 0, 0, 0.14), 0 1px 7px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -1px rgba(0, 0, 0, 0.2)`;
+    setTimeout(()=>{
+        btn.style.animation = `none`;
+    },500)
+    alert(`${province.value}+${number.value}`);
+    // console.log(province.value);
+    // console.log(number.value);
     // let btnStyle = document.getElementsByClassName('btn')[0].style;
     // btnStyle.backgroundColor = 'red';
 }
